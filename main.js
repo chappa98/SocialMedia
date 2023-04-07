2cd65c37b62aa9b9b3a5b583fad18d5e782a97f8
chappa98 <130122552+chappa98@users.noreply.github.com>
2023-04-06 23:54:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+const login=document.getElementById("loginpage");
+const register=-document.getElementById("registerpage");
+const post=document.getElementById("postpage");
+if(login) login.addEventListener('submit',functionlogin)
+if(register) register.addEventListener('submit',functionregister)
+if(post) note.addEventListener('submit',functionpost)
+
+
+function functionregister(e)
+{
+    e.preventDefault();
+    let firstname=document.getElementById("firstname").value;
+    let lastname=document.getElementById("lastname").value;
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+
+
 class User{
     constructor(firstname,lastname,email)
     {
@@ -29,5 +46,69 @@ class User{
         this.email=email;
     }
 }
-    const p1=new User("saikumar","Chappa","chappasaikumar6@gmail.com");
+    const p1=new User(firstname,lastname,email,password);
     console.log(p1);
+}
+
+function functionlogin(e)
+{
+    e.preventDefault()
+
+    let email=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+   
+    class User{
+        constructor(email,password)
+        {
+           
+            this.email=email;
+            this.password=password;
+        }
+
+        getemail()
+    {
+        return this.email;
+    }
+    getpassword()
+    {
+        return this.password;
+    }
+    setemail(email)
+    {
+        this.emailame=email;
+    }
+    setpassword(password)
+    {
+        this.password=password;
+    }
+       }
+   
+    const user1=new User(email,password);
+    console.log(user1);
+}
+
+function functionpost(e)
+{
+    e.preventDefault()
+   
+    let post=document.getElementById('post').value;
+   
+   
+    class User{
+        constructor(note)
+        {
+           
+            this.post=post;
+           
+        }
+        getpost(){
+            return this.post;
+        }
+        setpost(newpost){
+            this.post = newpost;
+        }
+    }
+
+    const user1=new User(post);
+    console.log(user1);
+}
